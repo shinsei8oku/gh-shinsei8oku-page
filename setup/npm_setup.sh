2ee2580189d9508cf9120c097288570961c23cfc
rita <k-otsuka@shinsei8oku.com>
2018-11-02 05:57:55 +0000

    deleted

--- setup/npm_setup.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get update              # 必要に応じてパッケージをアップデートする
-sudo apt-get install nodejs      # Node.js をインストールする
-sudo apt-get install npm         # npm をインストールする
-sudo update-alternatives --install /usr/bin/node node /usr/bin/nodejs 10  # update-alternatives を用いて nodejs を node に変更します
-npm install -g vue-cli
